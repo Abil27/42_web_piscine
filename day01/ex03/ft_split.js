07ea68a72496886d6d84d4adf6e352f93226eb68
Ahssimi mamadou Housseini <ahoussei@e1z4r9p18.42.us.org>
2018-12-26 02:40:34 -0800

    created the ft split function

--- day01/ex03/ft_split.js
+++ day01/ex03/ft_split.js
@@ -6,8 +6,26 @@
 /*   By: ahoussei <ahoussei@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/25 18:59:41 by ahoussei          #+#    #+#             */
-/*   Updated: 2018/12/25 21:23:05 by ahoussei         ###   ########.fr       */
+/*   Updated: 2018/12/26 02:39:09 by ahoussei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Réalisez la fonction ft_split. Elle prend une chaine de caractères en paramètre, et renvoie un tableau trié des différents mots, initialement séparés par un ou des espaces dans la chaine originale. Votre rendu ft_split.js sera inclus dans un fichier js de test*/
+
+
+let input = "   Hello    Houssein         Abil   "
+
+function ft_split(str){
+  return str.replace(/\s+/g, ' ').trim().split(' ').sort();
+}
+
+array = ft_split(input);
+
+console.log("Array");
+console.log("(");
+
+array.forEach(function (value, key){
+  console.log(`     [${key}] => ${value}`);
+})
+
+console.log(")");
