58fadd55262f5e9ac598d9d97c265352c8f22835
Houssein <milameet@gmail.com>
2018-12-28 21:30:03 -0800

    sing it

--- day01/ex13/sing_it!.js
+++ day01/ex13/sing_it!.js
@@ -1,17 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sing_it.js                                         :+:      :+:    :+:   */
+/*   sing_it!.js                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahoussei <ahoussei@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 13:58:11 by ahoussei          #+#    #+#             */
-/*   Updated: 2018/12/28 14:09:46 by ahoussei         ###   ########.fr       */
+/*   Updated: 2018/12/28 21:28:01 by ahoussei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-args = process.argv.slice(2);
+args = process.argv.slice(2).join('');
 
-  //userInput = data.toString().trim();
-  if(args === "mais pourquoi cette demo ?")
-    console.log("Tout simplement pour qu'en feuilletant le sujet")
+  if(args === "mais pourquoi cette demo ?"){
+    console.log("Tout simplement pour qu'en feuilletant le sujet on ne s'apercoive pas de la nature de l'exo");
+  } else if (args === "mais pourquoi cette chanson ?"){
+    console.log("Parce que Kwame a des enfants");
+  } else if (args === "vraiment ?"){
+    console.log("Nan c'est parce que c'est le premier avril")
+  }
+    
