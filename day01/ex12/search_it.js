d9c4cfed761b545e1ac3214be781fdd87eabb728
Houssein <milameet@gmail.com>
2018-12-30 10:24:37 -0800

    ex12

--- day01/ex12/search_it.js
+++ day01/ex12/search_it.js
@@ -6,10 +6,23 @@
 /*   By: ahoussei <ahoussei@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/29 14:02:05 by ahoussei          #+#    #+#             */
-/*   Updated: 2018/12/29 16:14:58 by ahoussei         ###   ########.fr       */
+/*   Updated: 2018/12/29 19:06:10 by ahoussei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*Votre but est de réaliser un programme qui affiche la valeur correspondante à une clef
 donnée comme premier paramètre, parmis les couples “clef :valeur” présents en nombre
 indéterminé passés en paramètres suivants */
+
+
+let args = process.argv.slice(2).join(' ');
+
+// console.log(args)
+
+let obj = {}
+for (let i = 0; i < args.length -1; i++){
+  let split = args[i].split(':');
+  obj[split[0].trim()] = split[1].trim()
+}
+
+console.log(obj)
