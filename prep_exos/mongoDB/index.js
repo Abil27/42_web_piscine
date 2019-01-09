3136a27171c6e50b8d6c8ee4138033ec328f18eb
Houssein Abil <milameet@gmail.com>
2019-01-08 20:11:40 -0800

    mongoDB exos

--- prep_exos/mongoDB/index.js
+++ prep_exos/mongoDB/index.js
@@ -6,7 +6,7 @@
 /*   By: ahoussei <ahoussei@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 13:58:55 by ahoussei          #+#    #+#             */
-/*   Updated: 2019/01/08 14:51:06 by ahoussei         ###   ########.fr       */
+/*   Updated: 2019/01/08 15:57:37 by ahoussei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,24 @@ async function createCourse() {
 }
 
 async function getCourse() {
+  // eq (equal)
+  // ne (not equal)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  //lte (less than or equal to)
+  // in
+  // nin ( not in)
+
   const course = await Course.find({ author: "Houssein", isPublished: true })
+    // .find({ price: {$gte: 10, $lte: 20 } })
+    // starts with Houssein
+    .find({ author: /^Houssein/i })
+    // Ends with Houssein
+    .find({ author: /Houssein$/i })
+    // contains Houssein
+    .find({ author: /.*Houssein.*/i })
+    .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
