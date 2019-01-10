58881c380994f8d0e897d4c308be45c909771415
Houssein Abil <milameet@gmail.com>
2019-01-09 16:42:51 -0800

    vidly database

--- prep_exos/small_apps/vidly/index.js
+++ prep_exos/small_apps/vidly/index.js
@@ -6,14 +6,21 @@
 /*   By: ahoussei <ahoussei@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 13:47:50 by ahoussei          #+#    #+#             */
-/*   Updated: 2019/01/08 13:47:51 by ahoussei         ###   ########.fr       */
+/*   Updated: 2019/01/09 16:37:20 by ahoussei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+const mongoose = require("mongoose");
 const Joi = require("joi");
 const genres = require("./routes/genres");
 const express = require("express");
 const app = express();
+
+// connect mongoose to our database
+mongoose
+  .connect("mongodb://locahost/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
 
 app.use(express.json());
 app.use("/api/genres", genres);
